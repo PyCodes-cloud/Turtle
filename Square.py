b1959653cc6d6b74957dddf3fe9ea03165370304
CodeTech <84615187+PyCodes-cloud@users.noreply.github.com>
2021-10-20 10:28:55 +0530

    Update Square.py

--- Square.py
+++ Square.py
@@ -3,10 +3,14 @@
 import turtle
 
 cursor = turtle.Turtle()
+# Setting shape of turtle cursor
 cursor.shape("circle")
+# Setting color of turtle cursor
 cursor.color("blue")
 
 # Movement for drawing a square using for loop!
 for n in range(4):
+    # It moves pointer forward 
     turtle.forward(100)
+    # rotates pointer to left by 90 degrees!
     turtle.left(90)
